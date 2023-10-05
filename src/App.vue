f997e8bbab4ff2c0adf25e83f1d561de4a868add
viktor_tumanov <viktor.tumanov@waveaccess.ru>
2023-10-05 13:40:52 +0300

    vue todo app

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,94 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-</template>
+<AppHeader/>
 
+  <AppFilters :active-filter="activeFilter" @set-filter="setFilter"/>
+  <main class="app-main">
+    <AppTodoList :todos="filteredTodos"
+                  @toggle-todo="toggleTodo"
+                  @remove-todo="removeTodo"/>
+
+    <AppAddTodo @add-todo="addTodo" />
+  </main>
+
+
+  <AppFooter :stats="stats"/>
+
+</template>
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppFilters from './components/AppFilters.vue';
+import AppTodoList from './components/AppTodoList.vue';
+import AppAddTodo from './components/AppAddTodo.vue';
+import  AppFooter, { Stats } from './components/AppFooter.vue';
+import { Todo } from './types/Todo';
+import { Filter } from './types/filter';
+
+interface State {
+  todos: Todo[],
+  activeFilter: Filter
+}
 
 export default defineComponent({
-  name: 'App',
   components: {
-    HelloWorld
+    AppHeader,
+    AppFilters,
+    AppTodoList,
+    AppAddTodo,
+    AppFooter
+  },
+  computed: {
+    filteredTodos(): Todo[] {
+      switch(this.activeFilter) {
+        case 'Active':
+          return  this.activeTodos
+        case 'Done':
+          return this.doneTodos
+        case 'All': 
+        default:
+          return this.todos
+      }
+    },
+    stats(): Stats{
+      return {
+        active: this.activeTodos.length,
+        done: this.doneTodos.length      
+      }
+    },
+    activeTodos(): Todo[] {
+      return  this.todos.filter(todo => !todo.completed)
+    },
+    doneTodos(): Todo[] {
+      return this.todos.filter(todo => todo.completed)
+    }
+  },
+  data(): State {
+    return {
+      todos: [
+      {id: 0, text:'Learn the basics of Vue', completed: false},
+      {id: 1, text:'Learn the basics of Typescript', completed: false},
+      {id: 2, text:'Subscribe to the channel', completed: false},
+      ],
+      activeFilter: 'All'
+    }
+  },
+  methods: {
+    addTodo(todo: Todo) {
+      this.todos.push(todo)
+    },
+    toggleTodo(id: number) {
+      const targetTodo = this.todos.find((todo:Todo) => todo.id === id )
+      
+      if(targetTodo) {
+        targetTodo.completed = !targetTodo.completed
+      }
+    },
+    removeTodo(id: number) {
+      this.todos = this.todos.filter((todo: Todo) => todo.id !== id)
+    },
+    setFilter(filter: Filter) {
+      this.activeFilter = filter
+    }
   }
-});
+})
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/AppFilters.vue
@@ -0,0 +1,45 @@
+<template>
+  <aside class="app-filters">
+    <section class="toggle-group">
+      <button 
+              v-for="filter in filters"
+              :key="filter"
+              :class="{'button--primary': activeFilter === filter}"
+              @click="setFilter(filter)"
+              class="button"
+              >{{ filter }}
+            </button>
+    </section>
+  </aside>
+</template>
+
+<script lang="ts">
+import { Filter } from '@/types/filter';
+import { PropType, defineComponent } from 'vue';
+
+interface State {
+  filters: Filter[]
+}
+
+export default defineComponent ({
+  props: {
+    activeFilter: {
+      type: String as PropType<Filter>,
+      required: true,
+    }
+  },
+  data(): State {
+    return {
+      filters: ['All', 'Active', 'Done']
+    }
+  },
+  methods: {
+    setFilter(filter: Filter) {
+      this.$emit('setFilter', filter)
+    }
+  },
+  emits: {
+    setFilter: (filter: Filter) => filter
+  }
+})
+</script>
